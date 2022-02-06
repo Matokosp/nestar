4a2d969af59b4e27bd30530206f3e67a239fd662
Matias Salinas <jmsalinas@uc.cl>
2022-02-06 15:28:38 -0500

    careers

---
+++ assets/js/careers/addJobs.js
@@ -0,0 +1,83 @@
+jobListEle = $('#jobs')
+jobListHiddenEle = $('#hiddenJobs')
+showListBtn = $('#showList')
+
+function myFunction(x) {
+    jobListEle.empty()
+    jobListHiddenEle.empty()
+    if (x.matches) {
+      let showing = jobs.slice(0, 5)
+      let hidden = jobs.slice(4 + 1)
+
+      showing.forEach(element => {
+        let position = 
+          `
+          <div class="job-item">
+              <div class="row">
+                  <div class="col-9">
+                      <div class="row align-items-center">
+                          <div class="col-12 col-md-6"><p class="large mb-0">${element.position}</p></div>
+                          <div class="col-12 col-md-6"><p class="mb-0">${element.category}</p></div>
+                      </div>
+                  </div>
+                  <div class="col-3 text-end d-flex justify-content-end align-items-end align-items-md-center">
+                      <a class="l400 m-0" href="${element.link}">Apply &nbsp&nbsp<span class="forward-arrow"><div></div></span></a>
+                  </div>
+              </div>
+          </div>
+          `
+      
+        jobListEle.append(position)
+    })
+    hidden.forEach(element => {
+        let position = 
+          `
+          <div class="job-item">
+              <div class="row">
+                  <div class="col-9">
+                      <div class="row align-items-center">
+                          <div class="col-12 col-md-6"><p class="large mb-0">${element.position}</p></div>
+                          <div class="col-12 col-md-6"><p class="mb-0">${element.category}</p></div>
+                      </div>
+                  </div>
+                  <div class="col-3 text-end d-flex justify-content-end align-items-end align-items-md-center">
+                      <a class="l400 m-0" href="${element.link}">Apply &nbsp&nbsp<span class="forward-arrow"><div></div></span></a>
+                  </div>
+              </div>
+          </div>
+          `
+      
+          jobListHiddenEle.append(position)
+    })
+    //   console.log({showing, hidden})
+    } else {
+        jobs.forEach(element => {
+            let position = 
+              `
+              <div class="job-item">
+                  <div class="row">
+                      <div class="col-9">
+                          <div class="row align-items-center">
+                              <div class="col-12 col-md-6"><p class="large mb-0">${element.position}</p></div>
+                              <div class="col-12 col-md-6"><p class="mb-0">${element.category}</p></div>
+                          </div>
+                      </div>
+                      <div class="col-3 text-end d-flex justify-content-end align-items-end align-items-md-center">
+                          <a class="l400 m-0 arrow-link" href="${element.link}">Apply &nbsp&nbsp<span class="forward-arrow"><div></div></span></a>
+                      </div>
+                  </div>
+              </div>
+              `
+          
+            jobListEle.append(position)
+        })
+    }
+  }
+
+  showListBtn.on('click', function() {
+    jobListHiddenEle.toggleClass('show-list')
+  })
+  
+  var md = window.matchMedia("(max-width: 768px)")
+  myFunction(md)
+  md.addListener(myFunction)
---
+++ assets/js/careers/jobsList.js
@@ -0,0 +1,37 @@
+const jobs = [
+    {
+        position: 'Project Manager',
+        category: 'Construction',
+        link : '#'
+    },
+    {
+        position: 'Assistance',
+        category: 'Construction',
+        link : '#'
+    },
+    {
+        position: 'Rent Accountant',
+        category: 'Accounting',
+        link : '#'
+    },
+    {
+        position: 'Analyst',
+        category: 'Controlling',
+        link : '#'
+    },
+    {
+        position: 'Assistant',
+        category: 'HR',
+        link : '#'
+    },
+    {
+        position: 'Operating cost accountant',
+        category: 'Property management',
+        link : '#'
+    },
+    {
+        position: 'Property manager',
+        category: 'Property management',
+        link : '#'
+    },
+]
